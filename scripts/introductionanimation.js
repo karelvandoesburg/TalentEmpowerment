c86fe429c570a311aca8a55edd8adf4de9850961
Karel van Doesburg <karelvandoesburg@gmail.com>
2017-09-29 16:56:52 +0200

    JS iets mooier gemaakt

--- scripts/introductionanimation.js
+++ scripts/introductionanimation.js
@@ -6,21 +6,32 @@ $(window).ready(function() {
 });
 
 function backgroundAnimation() {
+    moveDownCover();
+    moveDownPicture();
+    moveDownLogo();
+    moveDownText();
+}
+
+function moveDownCover() {
     $cover.velocity({
         'top' : screenHeight + 'px'
     }, 1500, 'easeInOutQuart')
-    
+}
+
+function moveDownPicture() {
     $intro.velocity({
         'top' : '0px'
     }, 1500, 'easeInOutQuart')
-    
+}
+
+function moveDownLogo() {
     $logo.velocity({
         'top' : '3%'
     }, {duration: 1300, easing: 'easeInOutQuart', delay: 600})
-    
+}
+
+function moveDownText() {
     $start.velocity({
         'top' : '25%'
     }, {duration: 1300, easing: 'easeInOutQuart', delay: 400})
-    
-    //color change $startbutton is done in CSS animations
 }
